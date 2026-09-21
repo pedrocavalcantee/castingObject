d898eeb6da3e78b385aa6be36fe39e612ffb7330
Pedro Cavalcante <phalvescavalcante@gmail.com>
2026-09-21 12:13:47 -0300

    Adiciona o desafio01 realizado no módulo Trabalhando com Listas e coleções de dados em Java

---
+++ desafio01/src/Principal.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Principal {
+    public static void main(String[] args) throws Exception {
+        //Criando a ArrayList e inicializando valores
+        ArrayList<String> nomes = new ArrayList<>();
+        nomes.add("Pedro");
+        nomes.add("Gui");
+        nomes.add("João");
+        nomes.add("Maria");
+        nomes.add("Iasmin");
+
+        //Percorrendo e imprimindo os valores da ArrayList
+        for (String nome : nomes) {
+            System.out.println("Nome: " + nome);
+        }
+
+    }
+}
